96140510bd6cac12a04b4b117f8ad1e7532a4779
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-08-09 20:30:41 -0500

    initial commit

---
+++ Chinook-SQLExercises/SQL-Files/sales_agent_invoices.sql
@@ -0,0 +1,7 @@
+﻿-- Provide a query that shows the invoices associated with each sales agent. 
+-- The resultant table should include the Sales Agent's full name.
+
+SELECT e.EmployeeId, e.FirstName, e.LastName, i.InvoiceId
+FROM Employee e
+JOIN InvoiceLine i
+ON e.EmployeeId = i.InvoiceId;
---
+++ Chinook-SQLExercises/SQL-Files/sales_agents.sql
@@ -0,0 +1,5 @@
+﻿-- Provide a query showing only the Employees who are Sales Agents
+
+SELECT FirstName, LastName, Title
+FROM Employee
+WHERE Title = 'Sales Support Agent';
