bfdd1fa9bead5300b3cfadf4ae39d8f0f50cbe80
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-08-12 10:30:27 -0500

    finished couple more queries, maybe 5 left

--- Chinook-SQLExercises/SQL-Files/top_2009_agent.sql
+++ Chinook-SQLExercises/SQL-Files/top_2009_agent.sql
@@ -1,2 +1,11 @@
 -- Which sales agent made the most in sales in 2009?
 -- Hint: Use the MAX function on a subquery
+
+(SELECT TOP 1 e.FirstName, e.LastName, SUM(i.Total) AS Sales
+FROM Employee e
+JOIN Customer c ON e.EmployeeId = c.SupportRepId
+JOIN Invoice i ON c.CustomerId = i.CustomerId
+WHERE e.Title LIKE '%Agent%' AND YEAR(i.InvoiceDate) = 2009
+GROUP BY e.EmployeeId, e.FirstName, e.LastName)
+ORDER BY e.EmployeeId DESC;
+
